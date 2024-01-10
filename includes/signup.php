318818e5bab6e72f8497090cc7d7c1213d919de9
Sam <60146956+srudqvist@users.noreply.github.com>
2024-01-10 09:13:46 -0600

    Displaying signup information
    Displaying the signup information temporarily before starting work on server side validation

--- includes/signup.php
+++ includes/signup.php
@@ -1,2 +1,35 @@
 <?php
 echo "hello0";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $facility = htmlspecialchars($_POST["facility"]);
+    $phone = htmlspecialchars($_POST["phone"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $password = htmlspecialchars($_POST["password"]);
+    $confirmPassword = htmlspecialchars($_POST["confirmPassword"]);
+
+    echo "Facility: ";
+    echo "<br>";
+    echo $facility;
+    echo "<br>";
+
+    echo "Phone: ";
+    echo "<br>";
+    echo $phone;
+    echo "<br>";
+
+    echo "Email: ";
+    echo "<br>";
+    echo $email;
+    echo "<br>";
+    echo "Password: ";
+    echo "<br>";
+    echo $password;
+    echo "<br>";
+
+    echo "Confirm Password: ";
+    echo "<br>";
+    echo $confirmPassword;
+} else {
+    header("Location: ../public/index.php");
+}
