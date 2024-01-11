f7292178a3411176f6f1c361d655ccab1f451ec1
Sam <60146956+srudqvist@users.noreply.github.com>
2024-01-11 14:44:22 -0600

    Styling changes and added color picker

--- public/eventContainers.php
+++ public/eventContainers.php
@@ -15,6 +15,7 @@
     <div id="eventContainers" class="container-row"></div>
 
     <div id="eventContainerModal" class="modal fade">
+
         <div class="modal-content">
             <span class="close">&times;</span>
             <div class="modal-header">
@@ -30,29 +31,33 @@
                     <input required type="text" name="eventName" id="eventName">
                     <div id="eventNameError"></div>
                     <div id="fromToDiv">
-                        <label for="dayFrom">From:</label>
-                        <select required name="dayFrom" id="dayFrom" placeholder="Monday">
-                            <option>Monday</option>
-                            <option>Tuesday</option>
-                            <option>Wednesday</option>
-                            <option>Thursday</option>
-                            <option>Friday</option>
-                            <option>Saturday</option>
-                            <option>Sunday</option>
-                        </select>
-                        <label for="dayTo">To:</label>
-                        <select required name="dayTo" id="dayTo" placeholder="Friday">
-                            <option>Monday</option>
-                            <option>Tuesday</option>
-                            <option>Wednesday</option>
-                            <option>Thursday</option>
-                            <option>Friday</option>
-                            <option>Saturday</option>
-                            <option>Sunday</option>
-                        </select>
-                        <label for="availableTimes">Available Times:</label>
-                        <input type="time" id="startTime" name="startTime">
-                        <input type="time" id="endTime" name="endTime">
+                        <div id="from">
+                            <label for="dayFrom">From:</label>
+                            <select required name="dayFrom" id="dayFrom" placeholder="Monday">
+                                <option>Monday</option>
+                                <option>Tuesday</option>
+                                <option>Wednesday</option>
+                                <option>Thursday</option>
+                                <option>Friday</option>
+                                <option>Saturday</option>
+                                <option>Sunday</option>
+                            </select>
+                            <input type="time" id="startTime" name="startTime">
+                        </div>
+                        <div id="to">
+                            <label for="dayTo">To:</label>
+                            <select required name="dayTo" id="dayTo" placeholder="Friday">
+                                <option>Monday</option>
+                                <option>Tuesday</option>
+                                <option>Wednesday</option>
+                                <option>Thursday</option>
+                                <option>Friday</option>
+                                <option>Saturday</option>
+                                <option>Sunday</option>
+                            </select>
+                            <!-- <label for="availableTimes">Available Times:</label> -->
+                            <input type="time" id="endTime" name="endTime">
+                        </div>
                     </div>
                     <label for="timeZone">Time Zone:</label>
                     <select required name="timeZone" id="timeZone" placeholder="">
@@ -66,14 +71,21 @@
                     </select>
                     <label for="description">Description:</label>
                     <textarea id="description" name="description" placeholder="Description"></textarea>
+                    <!--<div id="eventColorDiv">
+                        <label for="eventColor">Event Color:</label>
+                        <div class="color-picker">
+                            <div class="color-swatch" id="swatch1"></div>
+                            <div class="color-swatch" id="swatch2"></div>
+                            <div class="color-swatch" id="swatch3"></div>
+                        </div>
+                    </div>
+-->
+                    <label for="eventColor">Event Color:</label>
+                    <input type="color" id="eventColorPicker" name="eventColor" value="ff0000">
 
                     <div id="modalButtonsDiv">
                         <button id="cancelButton" type="button">Cancel</button>
                         <button type="submit">Create New Container</button>
-                    </div>
-                    <div id="eventColorDiv">
-                        <label for="eventColor">Event Color:</label>
-
                     </div>
                 </form>
             </div>
