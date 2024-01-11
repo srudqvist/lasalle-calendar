0ab5c3bbe927fb03a1992a6d37d60898e0871ece
Sam <60146956+srudqvist@users.noreply.github.com>
2024-01-11 08:24:57 -0600

    Factored out form styles.

--- public/forgotPassword.php
+++ public/forgotPassword.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/formStyles.css">
+    <link rel="stylesheet" href="css/forgotPassword.css">
     <title>Forgot Password</title>
 </head>
 
@@ -13,7 +14,7 @@
     <div id="navbarDiv">
         <?php include('navbar.php'); ?> <!-- Use require if it should error if navbar.php is not found -->
     </div>
-    <div id="loginForm">
+    <div id="forgotPassword">
         <form action="/includes/resetPassword.php" method="post" class="modern_form">
             <h1 class="center_content">Reset Password</h1>
             <label for="email">Email:</label>
