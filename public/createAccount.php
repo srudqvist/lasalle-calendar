73eb8e37416f53e8176237bd6ab477fc57f2b311
Sam <60146956+srudqvist@users.noreply.github.com>
2024-01-15 14:24:04 -0600

    fixed navbar-footer styling #1

--- public/createAccount.php
+++ public/createAccount.php
@@ -14,48 +14,50 @@
 </head>
 
 <body>
-    <div id="navbarDiv">
-        <?php include('navbar.php'); ?> <!-- Use require if it should error if navbar.php is not found -->
-    </div>
-    <div class="content">
-        <div id="signUp">
-            <form action="../includes/signup.php" method="post" id="signupForm" class="modern_form">
-                <h1 class="center_content">Sign Up</h1>
-                <label for="facility">Facility:</label>
-                <select required name="facility" id="facility" placeholder="facility">
-                    <option>Facility 1</option>
-                    <option>Facility 2</option>
-                    <option>Facility 3</option>
-                </select>
-                <label for="firstName">First Name:</label>
-                <input type="text" name="firstName" id="firstName">
-                <div id="firstNameError"></div>
-                <label for="lastName">Last Name:</label>
-                <input type="text" name="lastName" id="lastName">
-                <div id="lastNameError"></div>
-                <label for="phone">Phone:</label>
-                <input required type="text" name="phone" id="phone" placeholder="(xxx) xxx-xxxx">
-                <div id="phoneError"></div>
-                <label for="email">Email:</label>
-                <input required type="email" name="email" id="email" placeholder="example@test.com">
-                <div id="emailError"></div>
-                <label for="password">Password:
-                    <span class="password-tooltip"><i class="fas fa-info-circle"></i></span>
-                </label>
-                <input required type="password" name="password" id="password" placeholder="password123">
-                <div id="passwordError"></div>
-                <label for="confirmPassword">Confirm Password:</label>
-                <input required type="password" name="confirmPassword" id="confirmPassword" placeholder="password123">
-                <div id="confirmError"></div>
-                <button type="submit">Sign Up</button>
-                <div id="createForgot">
-                    <p>Already have an account? <a href="index.php">Log In</a></p>
-                </div>
-            </form>
+    <div class="flex-wrap">
+        <div id="navbarDiv">
+            <?php include('navbar.php'); ?> <!-- Use require if it should error if navbar.php is not found -->
         </div>
-    </div>
-    <div id="footerDiv">
-        <?php include('footer.php'); ?> <!-- Use require if it should error if footer.php is not found -->
+        <div class="content">
+            <div id="signUp">
+                <form action="../includes/signup.php" method="post" id="signupForm" class="modern_form">
+                    <h1 class="center_content">Sign Up</h1>
+                    <label for="facility">Facility:</label>
+                    <select required name="facility" id="facility" placeholder="facility">
+                        <option>Facility 1</option>
+                        <option>Facility 2</option>
+                        <option>Facility 3</option>
+                    </select>
+                    <label for="firstName">First Name:</label>
+                    <input type="text" name="firstName" id="firstName">
+                    <div id="firstNameError"></div>
+                    <label for="lastName">Last Name:</label>
+                    <input type="text" name="lastName" id="lastName">
+                    <div id="lastNameError"></div>
+                    <label for="phone">Phone:</label>
+                    <input required type="text" name="phone" id="phone" placeholder="(xxx) xxx-xxxx">
+                    <div id="phoneError"></div>
+                    <label for="email">Email:</label>
+                    <input required type="email" name="email" id="email" placeholder="example@test.com">
+                    <div id="emailError"></div>
+                    <label for="password">Password:
+                        <span class="password-tooltip"><i class="fas fa-info-circle"></i></span>
+                    </label>
+                    <input required type="password" name="password" id="password" placeholder="password123">
+                    <div id="passwordError"></div>
+                    <label for="confirmPassword">Confirm Password:</label>
+                    <input required type="password" name="confirmPassword" id="confirmPassword" placeholder="password123">
+                    <div id="confirmError"></div>
+                    <button type="submit">Sign Up</button>
+                    <div id="createForgot">
+                        <p>Already have an account? <a href="index.php">Log In</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div id="footerDiv">
+            <?php include('footer.php'); ?> <!-- Use require if it should error if footer.php is not found -->
+        </div>
     </div>
 </body>
 
