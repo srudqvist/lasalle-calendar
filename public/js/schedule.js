e93ec60dd07dcc1f9efabc1e26f96b132feab514
Sam <60146956+srudqvist@users.noreply.github.com>
2024-02-02 10:25:07 -0600

    Removed comment as instructed

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log("Cancel Button 0: " + cancelButtons[0].id);
   scheduleTable.addEventListener("click", function (event) {
     if (event.target.classList.contains("cancel-button")) {
-      // A button with class 'myButton' was clicked
       const buttonID = event.target.id;
       console.log("Button clicked:", buttonID);
       const meetingRowToCancel = document.getElementById(`meeting${buttonID}`);
