61e00f9cb368019c94b3c9616bf3cdbf01573a81
Sam <60146956+srudqvist@users.noreply.github.com>
2024-02-02 11:09:41 -0600

    Meeting row now disappears when confirm cancel is clicked

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -4,14 +4,24 @@ document.addEventListener("DOMContentLoaded", function () {
   const scheduleTable = document.getElementById("innerTable");
 
   console.log("Cancel Button 0: " + cancelButtons[0].id);
-  scheduleTable.addEventListener("click", function (event) {
+  scheduleTable.addEventListener("click", (event) => {
     if (event.target.classList.contains("cancel-button")) {
       const buttonID = event.target.id;
-      console.log("Button clicked:", buttonID);
       const meetingRowToCancel = document.getElementById(`meeting${buttonID}`);
       if (meetingRowToCancel) {
-        meetingRowToCancel.remove();
+        openCancelModal(meetingRowToCancel);
       }
     }
   });
+
+  function openCancelModal(meetingRowToCancel) {
+    deleteScheduleModal.style.display = "block";
+    const confirmCancel = document.getElementById("confirmCancel");
+    if (confirmCancel) {
+      confirmCancel.addEventListener("click", () => {
+        console.log("Confirm Cancel Clicked");
+        meetingRowToCancel.remove();
+      });
+    }
+  }
 });
