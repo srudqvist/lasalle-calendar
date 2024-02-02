58e5bab9e863a968b41fc3fc134fd1cecd107b9c
Sam <60146956+srudqvist@users.noreply.github.com>
2024-02-02 10:18:51 -0600

    Cancel button now deletes the correct row

--- public/schedule.php
+++ public/schedule.php
@@ -9,7 +9,9 @@
     <!-- <link rel="stylesheet" href="css/login.css"> -->
     <!-- <link rel="stylesheet" href="css/formStyles.css"> -->
     <link rel="stylesheet" href="css/schedule.css">
+    <link rel="stylesheet" href="./css/modal.css">
     <script src="./js/components/banner.js" defer></script>
+    <script src="./js/schedule.js" defer></script>
     <title>Virtual Attorney Visitation Online Calendar</title>
   </head>
 
@@ -28,7 +30,7 @@
           <tr>
             <td class="center_content">
               <br>
-              <table id="staff">
+              <table id="innerTable">
                 <tr>
                   <th class="green-background"><f1>Date</f1></th>
                   <th class="green-background"><f1>Time</f1></th>
@@ -47,12 +49,12 @@
                 array("2023-02-02", "02:30 PM", "Meeting B", "Jane Smith", "987-654-3210", "jane@example.com", "Additional notes", 2),
                 array("2023-02-03", "10:00 AM", "Meeting A", "John Smith", "123-456-7890", "john@example.com", "Some notes", 3),
                 array("2023-02-02", "02:30 PM", "Meeting B", "Jane Doe", "987-654-3210", "jane@example.com", "Additional notes", 4),
-                array("2023-02-01", "10:00 AM", "Meeting A", "John Doe", "123-456-7890", "john@example.com", "Some notes", 5),
+                array("2023-02-01", "10:00 AM", "Meeting A", "John Doe", "123-456-7890", "john@example.com", "Some notes that are quite long actually. I just wanted to see what it would look like. How will it wrap, if it even will wrap... qite long actually. I just wanted to see what it would look like. How will it wrap, if it even will wrap...", 5),
                 array("2023-02-02", "02:30 PM", "Meeting B", "Jane Smith", "987-654-3210", "jane@example.com", "Additional notes", 6),
                 );
 
       foreach ($fakeData as $row) { ?>
-                <tr>
+                <tr id="meeting<?php echo $row[7]?>">
                   <td><?php echo $row[0]; ?></td>
                   <td><?php echo $row[1]; ?></td>
                   <td><?php echo $row[2]; ?></td>
@@ -61,6 +63,7 @@
                   <td><a href="mailto:<?php echo $row[5]; ?>">&nbsp;<?php echo $row[5]; ?></a></td>
                   <td><td1><?php echo $row[6]; ?></td1></td>
                   <td><hypr><a href="del.html?id=<?php echo $row[7]; ?>" title="Delete" target="_blank">Delete</a></hypr></td>
+                  <td><button class="cancel-button" id="<?php echo $row[7]?>">Cancel</button>
                 </tr>
                 <?php } ?>
               </table>
@@ -70,6 +73,10 @@
         </table>
 
       </div>
+    <div id="scheduleModal" class="modal fade">
+      <div class="modal-content">
+      </div>
+        </div>
     </main>
     <div id="footerDiv">
       <?php include('footer.php'); ?> <!-- Use require if it should error if footer.php is not found -->
