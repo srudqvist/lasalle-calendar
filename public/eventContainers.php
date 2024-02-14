0e960769ca4e9f800e911d45db9c8850b299bf82
Sam <60146956+srudqvist@users.noreply.github.com>
2024-02-14 16:34:34 -0600

    Session works for logging in

--- public/eventContainers.php
+++ public/eventContainers.php
@@ -1,4 +1,13 @@
 <!DOCTYPE html>
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+    header('Location: index.php');
+    exit;
+}
+?>
 <html lang="en">
 
 <head>
@@ -12,116 +21,117 @@
     <title>Event Containers</title>
 </head>
 
-<body>
+  <body>
     <div id="navbarDiv">
       <?php include('navbar.php'); ?> <!-- Use require if it should error if navbar.php is not found  -->
     </div>
     <main>
-    <banner-component custom-text="Events | Scheduled" label="lasalle.user@lasallecorrections.com">
-    </banner-component>
-    <button id="addEventContainer">Add Event Container</button>
-    <div id="eventContainers" class="container-row"></div>
+<banner-component custom-text="Events | Scheduled" label="<?=htmlspecialchars($_SESSION['email'], ENT_QUOTES)?>">
+      </banner-component>
+      <p>Welcome back, <?=htmlspecialchars($_SESSION['fname'], ENT_QUOTES)?>!</p>
+      <button id="addEventContainer">Add Event Container</button>
+      <div id="eventContainers" class="container-row"></div>
 
-    <div id="eventContainerModal" class="modal fade">
+      <div id="eventContainerModal" class="modal fade">
 
         <div class="modal-content">
-            <div id="eventContainerModalFormDiv">
-                <form action="" method="post" id="eventContainerModalForm" class="modal-form">
-                    <span class="close">&times;</span>
-                    <h1 class="center_content">Create Event Container</h1>
-                    <div class="row">
-                        <label class="col-25" for="eventName">Event Name:</label>
-                        <input class="col-75" required type="text" name="eventName" id="eventName">
-                    </div>
-                    <div class="row" id="fromToDiv">
-                        <div id="from">
-                            <label class="col-25" for="dayFrom">Available Days:</label>
-                            <div class="col-75">
-                                <select class="col-75" required name="dayFrom" id="dayFrom" placeholder="Monday">
-                                    <option>Monday</option>
-                                    <option>Tuesday</option>
-                                    <option>Wednesday</option>
-                                    <option>Thursday</option>
-                                    <option>Friday</option>
-                                    <option>Saturday</option>
-                                    <option>Sunday</option>
-                                </select>
-                                <label for="dayTo"> - </label>
-                                <select class="col-75" required name="dayTo" id="dayTo" placeholder="Friday">
-                                    <option>Monday</option>
-                                    <option>Tuesday</option>
-                                    <option>Wednesday</option>
-                                    <option>Thursday</option>
-                                    <option>Friday</option>
-                                    <option>Saturday</option>
-                                    <option>Sunday</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div id="to">
-                            <!-- <label for="dayTo">To:</label> -->
+          <div id="eventContainerModalFormDiv">
+            <form action="" method="post" id="eventContainerModalForm" class="modal-form">
+              <span class="close">&times;</span>
+              <h1 class="center_content">Create Event Container</h1>
+              <div class="row">
+                <label class="col-25" for="eventName">Event Name:</label>
+                <input class="col-75" required type="text" name="eventName" id="eventName">
+              </div>
+              <div class="row" id="fromToDiv">
+                <div id="from">
+                  <label class="col-25" for="dayFrom">Available Days:</label>
+                  <div class="col-75">
+                    <select class="col-75" required name="dayFrom" id="dayFrom" placeholder="Monday">
+                      <option>Monday</option>
+                      <option>Tuesday</option>
+                      <option>Wednesday</option>
+                      <option>Thursday</option>
+                      <option>Friday</option>
+                      <option>Saturday</option>
+                      <option>Sunday</option>
+                    </select>
+                    <label for="dayTo"> - </label>
+                    <select class="col-75" required name="dayTo" id="dayTo" placeholder="Friday">
+                      <option>Monday</option>
+                      <option>Tuesday</option>
+                      <option>Wednesday</option>
+                      <option>Thursday</option>
+                      <option>Friday</option>
+                      <option>Saturday</option>
+                      <option>Sunday</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div id="to">
+                  <!-- <label for="dayTo">To:</label> -->
 
-                            <label class="col-25" for="availableTimes">Available Times:</label>
-                            <div class="col-75">
-                                <input required type="time" id="startTime" name="startTime">
-                                <label for="availableTimes"> - </label>
-                                <input required type="time" id="endTime" name="endTime">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <label class="col-25" for="timeZone">Duration:</label>
-                        <select class="col-75" required name="duration" id="duration" placeholder="">
-                            <option data-minutes="30">30 min</option>
-                            <option data-minutes="60">1h</option>
-                            <option data-minutes="90">1h 30 min</option>
-                            <option data-minutes="120">2h</option>
-                        </select>
-                    </div>
-                    <div class="row">
-                        <label class="col-25" for="timeZone">Time Zone:</label>
-                        <select class="col-75" required name="timeZone" id="timeZone" placeholder="">
-                            <option>Central Time Zone</option>
-                            <option>Eastern Time Zone</option>
-                            <option>Pacific Time Zone</option>
-                        </select>
-                    </div>
-                    <div class="row">
-                        <label class="col-25" for="meetingType">Meeting Type:</label>
-                        <select class="col-75" required name="meetingType" id="meetingType" placeholder="">
-                            <option>Zoom</option>
-                            <option>Teams</option>
-                            <option>Phone</option>
-                        </select>
-                    </div>
-                    <div class="row">
-                        <label class="col-25" for="description">Description:</label>
-                        <textarea class="col-75" id="description" name="description" placeholder="Description"></textarea>
-                    </div>
-                    <!--<div id="eventColorDiv">
-                        <label for="eventColor">Event Color:</label>
-                        <div class="color-picker">
-                            <div class="color-swatch" id="swatch1"></div>
-                            <div class="color-swatch" id="swatch2"></div>
-                            <div class="color-swatch" id="swatch3"></div>
-                        </div>
-                    </div>
+                  <label class="col-25" for="availableTimes">Available Times:</label>
+                  <div class="col-75">
+                    <input required type="time" id="startTime" name="startTime">
+                    <label for="availableTimes"> - </label>
+                    <input required type="time" id="endTime" name="endTime">
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <label class="col-25" for="timeZone">Duration:</label>
+                <select class="col-75" required name="duration" id="duration" placeholder="">
+                  <option data-minutes="30">30 min</option>
+                  <option data-minutes="60">1h</option>
+                  <option data-minutes="90">1h 30 min</option>
+                  <option data-minutes="120">2h</option>
+                </select>
+              </div>
+              <div class="row">
+                <label class="col-25" for="timeZone">Time Zone:</label>
+                <select class="col-75" required name="timeZone" id="timeZone" placeholder="">
+                  <option>Central Time Zone</option>
+                  <option>Eastern Time Zone</option>
+                  <option>Pacific Time Zone</option>
+                </select>
+              </div>
+              <div class="row">
+                <label class="col-25" for="meetingType">Meeting Type:</label>
+                <select class="col-75" required name="meetingType" id="meetingType" placeholder="">
+                  <option>Zoom</option>
+                  <option>Teams</option>
+                  <option>Phone</option>
+                </select>
+              </div>
+              <div class="row">
+                <label class="col-25" for="description">Description:</label>
+                <textarea class="col-75" id="description" name="description" placeholder="Description"></textarea>
+              </div>
+              <!--<div id="eventColorDiv">
+<label for="eventColor">Event Color:</label>
+<div class="color-picker">
+<div class="color-swatch" id="swatch1"></div>
+<div class="color-swatch" id="swatch2"></div>
+<div class="color-swatch" id="swatch3"></div>
+</div>
+</div>
 -->
-                    <div class="row">
-                        <div class="color-picker">
-                            <label class="col-25" for="eventColor">Event Color:</label>
-                            <input class="col-75" type="color" id="eventColorPicker" name="eventColor" value="#000000">
-                        </div>
-                    </div>
+              <div class="row">
+                <div class="color-picker">
+                  <label class="col-25" for="eventColor">Event Color:</label>
+                  <input class="col-75" type="color" id="eventColorPicker" name="eventColor" value="#000000">
+                </div>
+              </div>
 
-                    <div id="modalButtonsDiv">
-                        <button id="cancelButton" type="button">Cancel</button>
-                        <button id="submitButton" type="submit">Create New Container</button>
-                    </div>
-                </form>
-            </div>
+              <div id="modalButtonsDiv">
+                <button id="cancelButton" type="button">Cancel</button>
+                <button id="submitButton" type="submit">Create New Container</button>
+              </div>
+            </form>
+          </div>
         </div>
 
       </div> 
@@ -130,6 +140,6 @@
     <div id="footerDiv" class="footer">
       <?php include('footer.php'); ?> <!-- Use require if it should error if footer.php is not found -->
     </div>
-</body>
+  </body>
 
 </html>
