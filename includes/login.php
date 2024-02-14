2d67d95cc5674508c1bab5639a29e3de7ba96a3b
Sam <60146956+srudqvist@users.noreply.github.com>
2024-02-14 13:38:53 -0600

    db credentials working

--- includes/login.php
+++ includes/login.php
@@ -1,24 +1,36 @@
 <?php
 
+include '../../../lasalle-calendar-env-variables/config.php';
 // Make sure request is of the right type
 //var_dump($_SERVER["REQUEST_METHOD"]);
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email = htmlspecialchars($_POST["email"]);
     $password = htmlspecialchars($_POST["password"]);
 
-    // Todo: 
-    // * Handle invalid inputs before querying the db.
-    // * 
+    // Connect to the database
+    $mysqli = new mysqli($db_host, $db_username, $db_password, $db_database);
 
-    echo "Email: ";
-    echo "<br>";
-    echo $email;
-    echo "<br>";
-    echo "Password: ";
-    echo "<br>";
-    echo $password;
+    // Check for connection errors
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+    echo "Connection Successful";
+    // Prepare SQL query to retrieve user with the provided email
+    $stmt = $mysqli->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    //Echo the result
+    while ($row = $result->fetch_assoc()) {
+        echo "User ID: " . $row['uid'] . "<br>";
+        echo "Facility: " . $row['facility'] . "<br>";
+        echo "Email: " . $row['email'] . "<br>";
+        echo "First Name: " . $row['fname'] . "<br>";
+        echo "Last Name: " . $row['lname'] . "<br>";
+        echo "Phone: " . $row['phone'] . "<br>";
+    }
 
-    header("Location: ../public/eventContainers.php");
+    //header("Location: ../public/eventContainers.php");
 } else {
     header("Location: ../public/index.php");
 }
