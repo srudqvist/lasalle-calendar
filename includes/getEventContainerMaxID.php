09d2ec638cd90c2e96445db6677e5bada95f5616
Sam <60146956+srudqvist@users.noreply.github.com>
2024-02-16 12:52:02 -0600

    New event container has last ID

---
+++ includes/getEventContainerMaxID.php
@@ -0,0 +1,18 @@
+
+<?php
+include '../../../lasalle-calendar-env-variables/config.php';
+$conn = new mysqli($db_host, $db_username, $db_password, $db_database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT MAX(event_container_id) AS max_id FROM event_containers";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+$highestId = $row["max_id"];
+
+$conn->close();
+
+echo $highestId;
+?>
