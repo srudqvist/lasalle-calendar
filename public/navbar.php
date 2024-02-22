97033792e8310ba9b66f409fdc2501e4394ac238
Sam <60146956+srudqvist@users.noreply.github.com>
2024-02-22 10:36:19 -0600

    Clicking logo destroys session

--- public/navbar.php
+++ public/navbar.php
@@ -1,10 +1,10 @@
 <head>
-    <link rel="stylesheet" href="css/navbar.css">
+  <link rel="stylesheet" href="css/navbar.css">
 </head>
 <nav id="navbar">
-    <a href="index.php">
-        <img id="navbarLasalleLogo" src="img/LaSalleLogo.png" alt="lasalle logo">
-    </a>
-    <img id="calendarImg" src="img/cal.png" alt="calendar image">
-    <h1 id="navbarHeadline">Virtual Attorney Visitation Online Calendar</h1>
+  <a href="../../../includes/logout.php">
+    <img id="navbarLasalleLogo" src="img/LaSalleLogo.png" alt="lasalle logo">
+  </a>
+  <img id="calendarImg" src="img/cal.png" alt="calendar image">
+  <h1 id="navbarHeadline">Virtual Attorney Visitation Online Calendar</h1>
 </nav>
