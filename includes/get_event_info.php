6e1ec18d172483337a92c709040a81fb648817b7
Sam <60146956+srudqvist@users.noreply.github.com>
2024-02-22 10:05:12 -0600

    Put response header back

--- includes/get_event_info.php
+++ includes/get_event_info.php
@@ -26,7 +26,7 @@ if ($containerId) {
         $mysqli->close();
 
         // Return the data as JSON
-        //header('Content-Type: application/json');
+        header('Content-Type: application/json');
         echo json_encode($row);
     } else {
         // No matching container found
