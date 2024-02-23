6a2819a269241d149536aad5a075f62ec585d467
Sam <60146956+srudqvist@users.noreply.github.com>
2024-02-23 10:24:51 -0600

    Changing year if month is changed in dec and jan

--- public/js/calendar/submitDetails.js
+++ public/js/calendar/submitDetails.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-	const scheduleForm = document.getElementById("scheduleMeetingForm");
-	scheduleForm.addEventListener("submit", (event) => {
-		event.preventDefault();
-		// const dateTime = event.target.date.value;
-		// const timeZone = event.target.timezone.value;
-		const name = event.target.name.value;
-		const email = event.target.email.value;
-		const comments = event.target.comments.value;
+  const scheduleForm = document.getElementById("scheduleMeetingForm");
+  scheduleForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // const dateTime = event.target.date.value;
+    const timeZone = document.getElementById("timezone").innerText;
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const comments = event.target.comments.value;
 
-		console.log(`Name: ${name}, email: ${email} comments: ${comments}`);
-	});
+    console.log(
+      `Timezone: ${timeZone} Name: ${name}, email: ${email} comments: ${comments}`,
+    );
+  });
 });
