c3009941212b1e2f4ba6b13369c0a1f3ede7615d
Sam <60146956+srudqvist@users.noreply.github.com>
2024-02-28 12:23:50 -0600

    Styling changes to calendar

--- public/calendar.php
+++ public/calendar.php
@@ -10,7 +10,7 @@ session_start();
 ?>
 <html lang="en">
 
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
@@ -20,7 +20,7 @@ session_start();
     <script src="./js/calendar/submitDetails.js"></script>
     <script src="./js/components/banner.js" defer></script>
     <title>Calendar</title>
-</head>
+  </head>
 
   <body>
     <main>
@@ -32,38 +32,43 @@ session_start();
         </banner-component>
         <div id="contentDiv">
           <div id="meetingDetailsDiv">
-            <h1>Meeting Details</h1>
-            <div class="details-container">
-              <div class="label-column">
-                <label for="availableDays">Available Days:</label>
-                <label for="meetingType">Meeting Type:</label>
-                <label for="meetingDuration">Meeting Duration:</label>
-                <label for="meetingNotes">Meeting Notes:</label>
+            <div class="card">
+              <h1>Meeting Details</h1>
+              <div class="details-container">
+                <div class="label-column">
+                  <label for="availableDays">Available Days:</label>
+                  <label for="meetingType">Meeting Type:</label>
+                  <label for="meetingDuration">Meeting Duration:</label>
+                  <label for="meetingNotes">Meeting Notes:</label>
+                </div>
+                <div class="text-column">
+                  <span id="availableDays"></span>
+                  <span id="meetingType"></span>
+                  <span id="meetingDuration"></span>
+                  <span id="meetingNotes"></span>
+                </div>
               </div>
-              <div class="text-column">
-                <span id="availableDays"></span>
-                <span id="meetingType"></span>
-                <span id="meetingDuration"></span>
-                <span id="meetingNotes"></span>
-              </div>
+
             </div>
           </div>
 
           <div id="calendarDiv">
             <div id="calendarControls">
-              <div class="yearDiv gray-out">
+              <div class="yearDiv">
                 <button id="prevYear" class="calendar-button">
                   &larr; </button>
                 <span id="currentYear"></span>
                 <button id="nextYear" class="calendar-button"> &rarr;</button>
               </div>
-              <div class="monthDiv gray-out">
+              <div class="monthDiv">
                 <button id="prevMonth" class="calendar-button"> &larr;</button>
                 <span id="currentMonth"></span>
                 <button id="nextMonth" class="calendar-button"> &rarr;</button>
               </div>
             </div>
-            <div class="calendar" id="calendar"></div>
+            <div class="card">
+              <div class="calendar" id="calendar"></div>
+            </div>
             <div id="calendarTimezone"></div>
             <button id="nextButton" class="next-schedule-button">Next</button>
           </div>
@@ -72,40 +77,47 @@ session_start();
         </div>
         <div id="userDetailsDiv">
 
-          <div id="meetingDetailsDiv">
-            <div class="details-container">
-              <div class="label-column">
-                <label for="availableDays">Available Days:</label>
-                <label for="meetingType">Meeting Type:</label>
-                <label for="meetingDuration">Meeting Duration:</label>
-                <label for="meetingNotes">Meeting Notes:</label>
-              </div>
-              <div class="text-column">
-                <span id="availableDays2"></span>
-                <span id="meetingType2"></span>
-                <span id="meetingDuration2"></span>
-                <span id="meetingNotes2"></span>
+          <div id="meetingDetailsDiv2">
+            <div class="card">
+              <h1>Meeting Details</h1>
+              <div class="details-container">
+                <div class="label-column">
+                  <label for="availableDays">Available Days:</label>
+                  <label for="meetingType">Meeting Type:</label>
+                  <label for="meetingDuration">Meeting Duration:</label>
+                  <label for="meetingNotes">Meeting Notes:</label>
+                </div>
+                <div class="text-column">
+                  <span id="availableDays2"></span>
+                  <span id="meetingType2"></span>
+                  <span id="meetingDuration2"></span>
+                  <span id="meetingNotes2"></span>
+                </div>
               </div>
             </div>
           </div>
           <div id="meetingDetailsDiv2">
             <form id="scheduleMeetingForm">
-              <div class="details-container">
-                <div class="label-column">
-                  <label for="dateTime">Date/Time:</label>
-                  <label for="timeZone">Time Zone:</label>
-                  <label for="name">Name:</label>
-                  <label for="email">Email:</label>
+              <div class="card">
+                <div class="details-container">
+                  <div class="label-column">
+                    <label for="dateTime">Date/Time:</label>
+                    <label for="timeZone">Time Zone:</label>
+                    <label for="name">Name:</label>
+                    <label for="email">Email:</label>
+                  </div>
+                  <div class="text-column">
+                    <span id="date-time"></span>
+                    <span id="timezone"></span>
+                    <input type="text" id="name" name="name" required placeholder="John Doe">
+                    <input type="email" id="email" name="email" required placeholder="john.doe@email.com">
+                  </div>
                 </div>
-                <div class="text-column">
-                  <span id="date-time"></span>
-                  <span id="timezone"></span>
-                  <input type="text" id="name" name="name" required placeholder="John Doe">
-                  <input type="email" id="email" name="email" required placeholder="john.doe@email.com">
+                <div id="additionalCommentsDiv">
+                  <label id="additionalComments" for="comments">Additional Comments and Requests:</label>
+                  <textarea id="comments" name="comments"></textarea>
                 </div>
               </div>
-              <label id="additionalComments" for="comments">Additional Comments and Requests:</label>
-              <textarea id="comments" name="comments"></textarea>
               <div id="buttonDiv">
                 <button id="backButton" class="back-button">Back</button>
                 <button type="submit" id="scheduleButton" class="next-schedule-button">Schedule</button>
