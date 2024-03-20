9fa33da00a1f06e9b511ddb807c3706cdb93031a
Sam <60146956+srudqvist@users.noreply.github.com>
2024-03-20 13:03:33 -0500

    Styled user info

--- db/load_data.sql
+++ db/load_data.sql
@@ -1,7 +1,7 @@
 USE lasalle_virtual_db;
 
 INSERT INTO users (facility, email, secondary_email, first_name, last_name, phone, password) VALUES
-('', 'john.doe@example.com', 'secondatu.email@email.com', 'John', 'Doe', '123-456-7890', '$2y$10$pV2OWTWlyVvpTDeX7zAA5e0/LC5Jl7aiwiQwW5xKTNWrRpIGo4JLa'), 
+('Facility 1', 'john.doe@example.com', 'secondatu.email@email.com', 'John', 'Doe', '123-456-7890', '$2y$10$pV2OWTWlyVvpTDeX7zAA5e0/LC5Jl7aiwiQwW5xKTNWrRpIGo4JLa'), 
 ('Facility 1', 'jane.smith@example.com', NULL, 'Jane', 'Smith', '987-654-3210', '$2y$10$gjijKbtPTORm1Nn7KF7Eu.k32s/vmHzTE1z4AQGzQcKcK8pl5qhc2'), 
 ('Facility 2', 'robert.johnson@example.com', NULL, 'Robert', 'Johnson', '555-555-5555', '$2y$10$D2nNpUptp3qqsQ7NWcxBMO75/upgnJ3ZTXfXRXSpB25QPu7tfN9em'), 
 ('Facility 3', 'emily.wilson@example.com', NULL, 'Emily', 'Wilson', '555-555-5555', '$2y$10$k09oIyC12GkE2EiIjWmedO.qvkT1ytHKhYVlEWpIBcOzGvXUkXak6'); 
