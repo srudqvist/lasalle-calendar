8f4bac317d455539ee978f16e609ae31cb064b65
Sam <60146956+srudqvist@users.noreply.github.com>
2024-03-22 09:19:53 -0500

    Rewrote signup api, and added countdown for redirect

--- public/createAccount.php
+++ public/createAccount.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
@@ -11,54 +11,58 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <script src="./js/signupValidation.js"></script>
     <title>Sign Up</title>
-</head>
+  </head>
 
-<body>
+  <body>
     <div class="flex-wrap">
-        <div id="navbarDiv">
-            <?php include('navbar.php'); ?> <!-- Use require if it should error if navbar.php is not found -->
-        </div>
-        <div class="content">
-            <div id="signUp">
-                <form action="../includes/signup.php" method="post" id="signupForm" class="modern_form">
-                    <h1 class="center_content">Sign Up</h1>
-                    <label for="facility">Facility:</label>
-                    <select required name="facility" id="facility" placeholder="facility">
-                        <option>Facility 1</option>
-                        <option>Facility 2</option>
-                        <option>Facility 3</option>
-                    </select>
-                    <label for="firstName">First Name:</label>
-                    <input type="text" name="firstName" id="firstName">
-                    <div id="firstNameError"></div>
-                    <label for="lastName">Last Name:</label>
-                    <input type="text" name="lastName" id="lastName">
-                    <div id="lastNameError"></div>
-                    <label for="phone">Phone:</label>
-                    <input required type="text" name="phone" id="phone" placeholder="(xxx) xxx-xxxx">
-                    <div id="phoneError"></div>
-                    <label for="email">Email:</label>
-                    <input required type="email" name="email" id="email" placeholder="example@test.com">
-                    <div id="emailError"></div>
-                    <label for="password">Password:
-                        <span class="password-tooltip"><i class="fas fa-info-circle"></i></span>
-                    </label>
-                    <input required type="password" name="password" id="password" placeholder="password123">
-                    <div id="passwordError"></div>
-                    <label for="confirmPassword">Confirm Password:</label>
-                    <input required type="password" name="confirmPassword" id="confirmPassword" placeholder="password123">
-                    <div id="confirmError"></div>
-                    <button type="submit">Sign Up</button>
-                    <div id="createForgot">
-                        <p>Already have an account? <a href="index.php">Log In</a></p>
-                    </div>
-                </form>
+      <div id="navbarDiv">
+        <?php include('navbar.php'); ?> <!-- Use require if it should error if navbar.php is not found -->
+      </div>
+      <div class="content">
+        <div id="signUp">
+          <form action="../includes/signup.php" method="post" id="signupForm" class="modern_form">
+            <h1 class="center_content">Sign Up</h1>
+            <label for="facility">Facility:</label>
+            <select required name="facility" id="facility" placeholder="facility">
+              <option>Facility 1</option>
+              <option>Facility 2</option>
+              <option>Facility 3</option>
+            </select>
+            <label for="firstName">First Name:</label>
+            <input type="text" name="firstName" id="firstName">
+            <div id="firstNameError"></div>
+            <label for="lastName">Last Name:</label>
+            <input type="text" name="lastName" id="lastName">
+            <div id="lastNameError"></div>
+            <label for="phone">Phone:</label>
+            <input required type="text" name="phone" id="phone" placeholder="(xxx) xxx-xxxx">
+            <div id="phoneError"></div>
+            <label for="email">Email:</label>
+            <input required type="email" name="email" id="email" placeholder="example@test.com">
+            <div id="emailError"></div>
+            <label for="password">Password:
+              <span class="password-tooltip"><i class="fas fa-info-circle"></i></span>
+            </label>
+            <input required type="password" name="password" id="password" placeholder="password123">
+            <div id="passwordError"></div>
+            <label for="confirmPassword">Confirm Password:</label>
+            <input required type="password" name="confirmPassword" id="confirmPassword" placeholder="password123">
+            <div id="confirmError"></div>
+            <button type="submit">Sign Up</button>
+            <div id="successMessage">
+              <p>User Created</p>
+              <p id="redirectText"></p>
             </div>
+            <div id="createForgot">
+              <p>Already have an account? <a href="index.php">Log In</a></p>
+            </div>
+          </form>
         </div>
-        <div id="footerDiv">
-            <?php include('footer.php'); ?> <!-- Use require if it should error if footer.php is not found -->
-        </div>
+      </div>
+      <div id="footerDiv">
+        <?php include('footer.php'); ?> <!-- Use require if it should error if footer.php is not found -->
+      </div>
     </div>
-</body>
+  </body>
 
 </html>
--- public/js/signupValidation.js
+++ public/js/signupValidation.js
@@ -1,224 +1,257 @@
 document.addEventListener("DOMContentLoaded", function () {
-	const signupForm = document.getElementById("signupForm");
-	const passwordInput = document.getElementById("password");
-	const confirmPasswordInput = document.getElementById("confirmPassword");
-	//const errorMessagesDiv = document.getElementById("errorMessages");
-	//const existingErrorMessages = errorMessagesDiv.getElementsByTagName("span");
+  const signupForm = document.getElementById("signupForm");
+  const passwordInput = document.getElementById("password");
+  const confirmPasswordInput = document.getElementById("confirmPassword");
+  //const errorMessagesDiv = document.getElementById("errorMessages");
+  //const existingErrorMessages = errorMessagesDiv.getElementsByTagName("span");
 
-	const firstNameInput = document.getElementById("firstName");
-	const firstNameErrorDiv = document.getElementById("firstNameError");
-	const lastNameInput = document.getElementById("lastName");
-	const lastNameErrorDiv = document.getElementById("lastNameError");
-	const passwordErrorDiv = document.getElementById("passwordError");
-	const confirmPasswordErrorDiv = document.getElementById("confirmError");
+  const firstNameInput = document.getElementById("firstName");
+  const firstNameErrorDiv = document.getElementById("firstNameError");
+  const lastNameInput = document.getElementById("lastName");
+  const lastNameErrorDiv = document.getElementById("lastNameError");
+  const passwordErrorDiv = document.getElementById("passwordError");
+  const confirmPasswordErrorDiv = document.getElementById("confirmError");
 
-	const phoneInput = document.getElementById("phone");
+  const phoneInput = document.getElementById("phone");
 
-	if (signupForm) {
-		if (firstNameInput) {
-			firstNameInput.addEventListener("input", function (event) {
-				let currentValidation = nameValidation(event.target.value);
-				const errorMessage = "Name input failed validation";
-				if (!currentValidation) {
-					showErrorMessage(
-						errorMessage,
-						firstNameInput,
-						firstNameErrorDiv
-					);
-				} else if (
-					document.getElementById(firstNameErrorDiv.id + errorMessage)
-				) {
-					removeErrorMessage(
-						errorMessage,
-						firstNameInput,
-						firstNameErrorDiv
-					);
-				}
-			});
-		}
-		if (lastNameInput) {
-			lastNameInput.addEventListener("input", function (event) {
-				let currentValidation = nameValidation(event.target.value);
-				const errorMessage = "Name input failed validation";
-				if (!currentValidation) {
-					showErrorMessage(
-						errorMessage,
-						lastNameInput,
-						lastNameErrorDiv
-					);
-				} else if (
-					document.getElementById(lastNameErrorDiv.id + errorMessage)
-				) {
-					removeErrorMessage(
-						errorMessage,
-						lastNameInput,
-						lastNameErrorDiv
-					);
-				}
-			});
-		}
-		if (phoneInput) {
-			// Handle the phone number format as the user is typing.
-			phoneInput.addEventListener("input", function (event) {
-				const isBackspace = event.inputType === "deleteContentBackward";
-				if (!isBackspace) {
-					let inputValue = event.target.value.replace(/\D/g, "");
-					if (inputValue.length > 0) {
-						inputValue =
-							"(" +
-							inputValue.substring(0, 3) +
-							") " +
-							inputValue.substring(3);
-					}
-					if (inputValue.length > 8) {
-						inputValue =
-							inputValue.substring(0, 9) +
-							"-" +
-							inputValue.substring(9);
-					}
-					if (inputValue.length > 14) {
-						inputValue = inputValue.substring(0, 14);
-					}
-					event.target.value = inputValue;
-				} else {
-					let inputValue = event.target.value;
-					const lastChar = inputValue.charAt(inputValue.length - 1);
-					if (lastChar === ")" || lastChar === "-") {
-						inputValue = inputValue.substring(
-							0,
-							inputValue.length - 1
-						);
-					}
-					event.target.value = inputValue;
-				}
-			});
-		}
-		if (passwordInput) {
-			passwordInput.addEventListener("input", function (event) {
-				// let currentValidation = nameValidation(event.target.value);
-				const errorMessage =
-					"Password not meeting complexity requirements";
-				if (!isPasswordValid(event.target.value)) {
-					showErrorMessage(
-						errorMessage,
-						passwordInput,
-						passwordErrorDiv
-					);
-				} else if (
-					document.getElementById(passwordErrorDiv.id + errorMessage)
-				) {
-					removeErrorMessage(
-						errorMessage,
-						passwordInput,
-						passwordErrorDiv
-					);
-				}
-			});
-		}
-		if (passwordInput && confirmPasswordInput) {
-			confirmPasswordInput.addEventListener("input", function (event) {
-				const errorMessage = "Passwords do not match";
-				if (passwordInput.value !== event.target.value) {
-					showErrorMessage(
-						errorMessage,
-						confirmPasswordInput,
-						confirmPasswordErrorDiv
-					);
-				} else if (
-					document.getElementById(
-						confirmPasswordErrorDiv.id + errorMessage
-					)
-				) {
-					removeErrorMessage(
-						errorMessage,
-						confirmPasswordInput,
-						confirmPasswordErrorDiv
-					);
-				}
-			});
-		}
+  if (signupForm) {
+    if (firstNameInput) {
+      firstNameInput.addEventListener("input", function (event) {
+        let currentValidation = nameValidation(event.target.value);
+        const errorMessage = "Name input failed validation";
+        if (!currentValidation) {
+          showErrorMessage(errorMessage, firstNameInput, firstNameErrorDiv);
+        } else if (
+          document.getElementById(firstNameErrorDiv.id + errorMessage)
+        ) {
+          removeErrorMessage(errorMessage, firstNameInput, firstNameErrorDiv);
+        }
+      });
+    }
+    if (lastNameInput) {
+      lastNameInput.addEventListener("input", function (event) {
+        let currentValidation = nameValidation(event.target.value);
+        const errorMessage = "Name input failed validation";
+        if (!currentValidation) {
+          showErrorMessage(errorMessage, lastNameInput, lastNameErrorDiv);
+        } else if (
+          document.getElementById(lastNameErrorDiv.id + errorMessage)
+        ) {
+          removeErrorMessage(errorMessage, lastNameInput, lastNameErrorDiv);
+        }
+      });
+    }
+    if (phoneInput) {
+      // Handle the phone number format as the user is typing.
+      phoneInput.addEventListener("input", function (event) {
+        const isBackspace = event.inputType === "deleteContentBackward";
+        if (!isBackspace) {
+          let inputValue = event.target.value.replace(/\D/g, "");
+          if (inputValue.length > 0) {
+            inputValue =
+              "(" + inputValue.substring(0, 3) + ") " + inputValue.substring(3);
+          }
+          if (inputValue.length > 8) {
+            inputValue =
+              inputValue.substring(0, 9) + "-" + inputValue.substring(9);
+          }
+          if (inputValue.length > 14) {
+            inputValue = inputValue.substring(0, 14);
+          }
+          event.target.value = inputValue;
+        } else {
+          let inputValue = event.target.value;
+          const lastChar = inputValue.charAt(inputValue.length - 1);
+          if (lastChar === ")" || lastChar === "-") {
+            inputValue = inputValue.substring(0, inputValue.length - 1);
+          }
+          event.target.value = inputValue;
+        }
+      });
+    }
+    if (passwordInput) {
+      passwordInput.addEventListener("input", function (event) {
+        // let currentValidation = nameValidation(event.target.value);
+        const errorMessage = "Password not meeting complexity requirements";
+        if (!isPasswordValid(event.target.value)) {
+          showErrorMessage(errorMessage, passwordInput, passwordErrorDiv);
+        } else if (
+          document.getElementById(passwordErrorDiv.id + errorMessage)
+        ) {
+          removeErrorMessage(errorMessage, passwordInput, passwordErrorDiv);
+        }
+      });
+    }
+    if (passwordInput && confirmPasswordInput) {
+      confirmPasswordInput.addEventListener("input", function (event) {
+        const errorMessage = "Passwords do not match";
+        if (passwordInput.value !== event.target.value) {
+          showErrorMessage(
+            errorMessage,
+            confirmPasswordInput,
+            confirmPasswordErrorDiv,
+          );
+        } else if (
+          document.getElementById(confirmPasswordErrorDiv.id + errorMessage)
+        ) {
+          removeErrorMessage(
+            errorMessage,
+            confirmPasswordInput,
+            confirmPasswordErrorDiv,
+          );
+        }
+      });
+    }
 
-		signupForm.addEventListener("submit", function (event) {
-			event.preventDefault();
-			const password = document.getElementById("password").value;
-			const confirmPassword =
-				document.getElementById("confirmPassword").value;
+    signupForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+      const password = document.getElementById("password").value;
+      const confirmPassword = document.getElementById("confirmPassword").value;
 
-			if (
-				nameValidation(firstNameInput.value) &&
-				nameValidation(lastNameInput.value) &&
-				isPasswordValid(password) &&
-				password === confirmPassword
-			) {
-				signupForm.submit();
-			} else {
-				// ? Display bigger error message?
-			}
-		});
-	}
-	//Add password requirements text, show on hover, hide otherwise.
-	const tooltip = document.querySelector(".password-tooltip");
+      if (
+        nameValidation(firstNameInput.value) &&
+        nameValidation(lastNameInput.value) &&
+        isPasswordValid(password) &&
+        password === confirmPassword
+      ) {
+        //signupForm.submit();
+        const formData = {
+          facility: event.target.facility.value,
+          firstName: event.target.firstName.value,
+          lastName: event.target.lastName.value,
+          phone: event.target.phone.value,
+          email: event.target.email.value,
+          password: event.target.password.value,
+          confirmPassword: event.target.confirmPassword.value,
+        };
+        const responseMessage = submitSignupForm(formData);
+        console.log(responseMessage);
+        if (responseMessage) {
+        }
+      } else {
+        // ? Display bigger error message?
+      }
+    });
+  }
+  //Add password requirements text, show on hover, hide otherwise.
+  const tooltip = document.querySelector(".password-tooltip");
 
-	tooltip.addEventListener("mouseenter", function () {
-		const tooltipText = document.createElement("span");
-		tooltipText.textContent =
-			"Password must contain at least 8 characters, including at least one uppercase letter, one lowercase letter, one number, and one special character.I";
-		tooltipText.classList.add("tooltiptext");
-		this.appendChild(tooltipText);
-	});
+  tooltip.addEventListener("mouseenter", function () {
+    const tooltipText = document.createElement("span");
+    tooltipText.textContent =
+      "Password must contain at least 8 characters, including at least one uppercase letter, one lowercase letter, one number, and one special character.I";
+    tooltipText.classList.add("tooltiptext");
+    this.appendChild(tooltipText);
+  });
 
-	tooltip.addEventListener("mouseleave", function () {
-		const tooltipText = document.querySelector(".tooltiptext");
-		this.removeChild(tooltipText);
-	});
+  tooltip.addEventListener("mouseleave", function () {
+    const tooltipText = document.querySelector(".tooltiptext");
+    this.removeChild(tooltipText);
+  });
 
-	function nameValidation(name) {
-		console.log(name);
-		// Regex allowing letters, spaces, hyphens, and apostrophes
-		if (name.length > 0) {
-			const nameRegex = /^[A-Za-z]+(?:[-' ]?[A-Za-z]+)*$/;
-			const validName = nameRegex.test(name);
-			return validName;
-		} else {
-			return true;
-		}
-	}
+  function nameValidation(name) {
+    console.log(name);
+    // Regex allowing letters, spaces, hyphens, and apostrophes
+    if (name.length > 0) {
+      const nameRegex = /^[A-Za-z]+(?:[-' ]?[A-Za-z]+)*$/;
+      const validName = nameRegex.test(name);
+      return validName;
+    } else {
+      return true;
+    }
+  }
 
-	function showErrorMessage(message, inputName, errorDiv) {
-		//inputName.classList.remove("");
-		const errorAlreadyExists = document.getElementById(
-			errorDiv.id + message
-		);
-		if (!errorAlreadyExists) {
-			let errorMessagesDiv = errorDiv;
-			const errorMessage = document.createElement("span");
+  function showErrorMessage(message, inputName, errorDiv) {
+    //inputName.classList.remove("");
+    const errorAlreadyExists = document.getElementById(errorDiv.id + message);
+    if (!errorAlreadyExists) {
+      let errorMessagesDiv = errorDiv;
+      const errorMessage = document.createElement("span");
 
-			inputName.classList.add("input-error-red-border");
-			inputName.style.outline = "2px solid red";
+      inputName.classList.add("input-error-red-border");
+      inputName.style.outline = "2px solid red";
 
-			errorDiv.classList.add("password-no-match");
+      errorDiv.classList.add("password-no-match");
 
-			errorMessage.id = errorDiv.id + message;
-			errorMessage.textContent = message;
-			errorMessage.classList.add("red_text");
+      errorMessage.id = errorDiv.id + message;
+      errorMessage.textContent = message;
+      errorMessage.classList.add("red_text");
 
-			errorMessagesDiv.appendChild(errorMessage);
-		}
-	}
-	function removeErrorMessage(message, inputName, errorDiv) {
-		const errorToRemove = document.getElementById(errorDiv.id + message);
-		inputName.style.outline = "";
-		inputName.classList.remove("input-error-red-border");
-		errorToRemove.remove();
-	}
+      errorMessagesDiv.appendChild(errorMessage);
+    }
+  }
+  function removeErrorMessage(message, inputName, errorDiv) {
+    const errorToRemove = document.getElementById(errorDiv.id + message);
+    inputName.style.outline = "";
+    inputName.classList.remove("input-error-red-border");
+    errorToRemove.remove();
+  }
 
-	function isPasswordValid(password) {
-		if (password.length > 0) {
-			const passwordRegex =
-				/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-			return passwordRegex.test(password);
-		} else {
-			return true;
-		}
-	}
+  function isPasswordValid(password) {
+    if (password.length > 0) {
+      const passwordRegex =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+      return passwordRegex.test(password);
+    } else {
+      return true;
+    }
+  }
 });
+
+async function submitSignupForm(requestData) {
+  console.log(requestData);
+  try {
+    const url = "../../includes/signup.php";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestData),
+    });
+
+    if (!response.ok) {
+      console.log(response);
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (data) {
+      console.log(`DATA: ${data["message"]}`);
+      if (data["success"] === true) {
+        const successMessage = document.getElementById("successMessage");
+        successMessage.style.display = "block";
+
+        var targetTime = new Date().getTime() + 3000;
+        updateCountdown(targetTime);
+      }
+      return data;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+// Function to update the countdown
+function updateCountdown(targetTime) {
+  var intervalId; // Declare intervalId variable outside of the function
+  var intervalId = setInterval(function () {
+    var now = new Date().getTime();
+    var timeDifference = targetTime - now;
+
+    // Calculate remaining time in seconds
+    var seconds = Math.ceil(timeDifference / 1000);
+
+    // Display the remaining time on the page
+    document.getElementById("redirectText").textContent =
+      "Redirecting in " + seconds + " seconds";
+
+    // If time difference is less than or equal to 0, stop updating the countdown and perform the redirection
+    if (timeDifference <= 0) {
+      clearInterval(intervalId); // Stop the interval
+      window.location.replace("index.php");
+    }
+  }, 1000);
+}
