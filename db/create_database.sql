b3c3719af017ed0a44b977233add611b44ddc01d
Sam <60146956+srudqvist@users.noreply.github.com>
2024-04-04 14:10:39 -0500

    Added bool for tracking password reset

--- db/create_database.sql
+++ db/create_database.sql
@@ -12,7 +12,8 @@ CREATE TABLE IF NOT EXISTS users (
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     phone VARCHAR(15) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    password_reset BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 -- Create the table for the event containers
