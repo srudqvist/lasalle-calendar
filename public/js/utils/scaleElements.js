8217aa422dd3c3123faf2bc7209c9a5dca4b17ba
Sam <60146956+srudqvist@users.noreply.github.com>
2024-04-05 13:14:22 -0500

    Disabled buttons when password reset

--- public/js/utils/scaleElements.js
+++ public/js/utils/scaleElements.js
@@ -1,11 +1,5 @@
 function scaleUpElement(element, scaleFactor = 1.1) {
-  console.log("scaling up");
-  console.log(element);
-  console.log(element.tagName);
   if (element.tagName == "BUTTON" && element.disabled) {
-    console.log("here");
-    console.log(element);
-    console.log(element.disabled);
   } else if (element.tagName === "TD") {
     element.style.transform = "scale(1.2)";
   } else {
@@ -15,11 +9,7 @@ function scaleUpElement(element, scaleFactor = 1.1) {
 }
 
 function resetScaleElement(element, scaleFactor = 1.0) {
-  if (element.tagName == "BUTTON" && element.disabled) {
-    console.log("here");
-    console.log(element);
-    console.log(element.disabled);
-  } else {
+  if (!(element.tagName == "BUTTON" && element.disabled)) {
     element.style.transform = `scale(${scaleFactor})`;
     element.style.transition = "transform 0.1s ease";
   }
