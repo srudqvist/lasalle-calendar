d298aad182d414b69510846627cf74a8922e639f
Sam <60146956+srudqvist@users.noreply.github.com>
2024-04-05 12:49:39 -0500

    Changed eventhandler on edit icon

--- public/js/utils/scaleElements.js
+++ public/js/utils/scaleElements.js
@@ -1,5 +1,12 @@
 function scaleUpElement(element, scaleFactor = 1.1) {
-  if (element.tagName === "TD") {
+  console.log("scaling up");
+  console.log(element);
+  console.log(element.tagName);
+  if (element.tagName == "BUTTON" && element.disabled) {
+    console.log("here");
+    console.log(element);
+    console.log(element.disabled);
+  } else if (element.tagName === "TD") {
     element.style.transform = "scale(1.2)";
   } else {
     element.style.transform = `scale(${scaleFactor})`;
@@ -7,9 +14,15 @@ function scaleUpElement(element, scaleFactor = 1.1) {
   element.style.transition = "transform 0.1s ease";
 }
 
-function resetScaleElement(element) {
-  element.style.transform = "scale(1)";
-  element.style.transition = "transform 0.1s ease";
+function resetScaleElement(element, scaleFactor = 1.0) {
+  if (element.tagName == "BUTTON" && element.disabled) {
+    console.log("here");
+    console.log(element);
+    console.log(element.disabled);
+  } else {
+    element.style.transform = `scale(${scaleFactor})`;
+    element.style.transition = "transform 0.1s ease";
+  }
 }
 
 export { scaleUpElement, resetScaleElement };
